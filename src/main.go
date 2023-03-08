b4ddadfe1476b2ff50618d8247dab399587b7475
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-03-08 17:13:50 +0100

    test

--- src/main.go
+++ src/main.go
@@ -20,20 +20,21 @@ type Bokoblin struct {
 		ID              int      `json:"id"`
 		Image           string   `json:"image"`
 		Name            string   `json:"name"`
+		Type            string
 	} `json:"data"`
-	Bokob      *Bokoblin
 	Formulaire string
 	Type_mob   []string
+	Bokob      *Bokoblin
 }
 
 func main() {
 
 	var b *Bokoblin
-	// fs := http.FileServer(http.Dir("css"))
-	// http.Handle("/css/", http.StripPrefix("/css/", fs))
-	// http.HandleFunc("/", b.OpenPage)
-	// http.ListenAndServe(":8080", nil)
-	fmt.Println(b.Create(b.type_mob()))
+	fs := http.FileServer(http.Dir("css"))
+	http.Handle("/css/", http.StripPrefix("/css/", fs))
+	http.HandleFunc("/", b.OpenPage)
+	http.ListenAndServe(":8080", nil)
+	// fmt.Println(b.type_mob())
 }
 
 func (b *Bokoblin) OpenPage(w http.ResponseWriter, r *http.Request) {
@@ -84,42 +85,4 @@ func (b *Bokoblin) type_mob() []string {
 		}
 	}
 	return result
-}
-
-func (b *Bokoblin) Create(links []string) {
-	// Mettre ici la liste des liens JSON à récupérer
-	urls := links
-
-	var combinedData []*Bokoblin
-
-	for _, url := range urls {
-		// Récupération du contenu JSON
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Printf("Erreur lors de la récupération du lien %s : %s\n", url, err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		// Lecture du contenu JSON
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Printf("Erreur lors de la lecture du contenu JSON du lien %s : %s\n", url, err)
-			continue
-		}
-
-		// Décodage JSON
-		var data []*Bokoblin
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			fmt.Printf("Erreur lors du décodage JSON du lien %s : %s\n", url, err)
-			continue
-		}
-
-		// Ajout des données décodées à la structure combinée
-		combinedData = append(combinedData, data...)
-	}
-
-	// Faire quelque chose avec la structure combinée
-	fmt.Printf("Nombre total d'éléments : %d\n", len(combinedData))
 }
