1b53b9148acf478d06816c443f9579f9db1a136d
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2023-03-13 15:50:39 +0100

    Creation pages de liens

--- src/main.go
+++ src/main.go
@@ -19,6 +19,7 @@ type Monster struct {
 	Image           string   `json:"image"`
 	Name            string   `json:"name"`
 	Type            string
+	Type_bis        string
 }
 
 type MonsterRequest struct {
@@ -33,12 +34,17 @@ func main() {
 	b.Init()
 	fs := http.FileServer(http.Dir("css"))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
-	http.HandleFunc("/", b.OpenPage)
+	http.HandleFunc("/", OpenPageIndex)
+	http.HandleFunc("/mob", b.OpenPage)
 	http.ListenAndServe(":8080", nil)
+}
+func OpenPageIndex(w http.ResponseWriter, r *http.Request) {
+	tmp := template.Must(template.ParseFiles("index.html"))
+	tmp.Execute(w, nil)
 }
 
 func (b *MonsterRequest) OpenPage(w http.ResponseWriter, r *http.Request) {
-	tmp := template.Must(template.ParseFiles("index.html"))
+	tmp := template.Must(template.ParseFiles("mob.html"))
 	details := MonsterRequest{
 		Formulaire: r.FormValue("nom"),
 		Capa:       r.FormValue("information"),
@@ -78,12 +84,16 @@ func (b *MonsterRequest) TypeMonsters() {
 	for i := 0; i < len(b.Monsters); i++ {
 		if strings.Contains(b.Monsters[i].Name, "fire") || strings.Contains(b.Monsters[i].Name, "igneo") || strings.Contains(b.Monsters[i].Name, "meteo") {
 			b.Monsters[i].Type = "fire"
-		} else if strings.Contains(b.Monsters[i].Name, "ice") || strings.Contains(b.Monsters[i].Name, "snow") || strings.Contains(b.Monsters[i].Name, "frost") || strings.Contains(b.Monsters[i].Name, "blizz") {
+		} else if strings.Contains(b.Monsters[i].Name, "ice") || strings.Contains(b.Monsters[i].Name, "snow") || strings.Contains(b.Monsters[i].Name, "frost") || strings.Contains(b.Monsters[i].Name, "blizz") || strings.Contains(b.Monsters[i].Name, "waterblight") || strings.Contains(b.Monsters[i].Name, "octorok") {
 			b.Monsters[i].Type = "ice"
-		} else if strings.Contains(b.Monsters[i].Name, "electric") {
+		} else if strings.Contains(b.Monsters[i].Name, "electric") || strings.Contains(b.Monsters[i].Name, "thunder") {
 			b.Monsters[i].Type = "electric"
 		} else if strings.Contains(b.Monsters[i].Name, "cursed") {
 			b.Monsters[i].Type = "cursed"
+		} else if strings.Contains(b.Monsters[i].Name, "guardian") {
+			b.Monsters[i].Type = "guardian"
+		} else if strings.Contains(b.Monsters[i].Name, "stal") {
+			b.Monsters[i].Type = "stal"
 		}
 
 	}
